4603fb51e74d74a617df19cdb4732a1c16f69648
Arnab Chowdhury <arnabthe07@gmail.com>
2025-08-17 16:56:13 +0530

    First commit: Initial project setup

---
+++ src/main/java/com/example/personalFiManager/PersonalFiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalFiManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFiManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personalFiManager/PersonalFiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalFiManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFiManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
